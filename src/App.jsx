6c447d740135f1fbc5a35a1c986f6d02f0c36ea4
1974mds <1974mds@gmail.com>
2026-01-18 04:52:32 +0000

    fix(deploy): Use HashRouter for Cloudflare Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ResultPage from './pages/ResultPage';
 
@@ -7,12 +7,12 @@ function App() {
   const [image, setImage] = useState(null);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage setImage={setImage} />} />
         <Route path="/result" element={<ResultPage image={image} setImage={setImage} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
